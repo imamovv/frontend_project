a2ac71883e176b373d7fbe4ed90b541fd983be76
imamovv <imamovv@gmail.com>
2024-05-30 22:26:55 +0300

    practice-2

--- src/components/MyHeader/MyHeader.tsx
+++ src/components/MyHeader/MyHeader.tsx
@@ -1,9 +1,9 @@
 import {useAuthContext} from "../../context/authContext";
-import {useNavigate} from "react-router-dom";
-import {NavLink} from "react-router-dom";
+import {NavLink, useNavigate, useLocation} from "react-router-dom";
 import {Button, Flex, Menu, MenuProps} from "antd"
 
 export default function MyHeader() {
+    const location = useLocation();
     const navigate = useNavigate();
     const {isLogin, user, logout} = useAuthContext()
 
@@ -16,24 +16,28 @@ export default function MyHeader() {
 
     let topNavItems: MenuProps['items'] = [
         {
-            key: 'home',
+            key: '/',
             label: <NavLink to="/">Главная</NavLink>
         },
     ];
 
     if (isLogin) {
         topNavItems.push({
-            key: 'pollution',
+            key: '/pollution',
             label: <NavLink to="/pollution">Загрязнения</NavLink>
+        })
+        topNavItems.push({
+            key: '/city',
+            label: <NavLink to="/city">Информация о городе</NavLink>
         })
     } else {
         topNavItems.push(...[
             {
-                key: 'login',
+                key: '/login',
                 label: <NavLink to="/login">Вход</NavLink>
             },
             {
-                key: 'registration',
+                key: '/registration',
                 label: <NavLink to="/registration">Регистрация</NavLink>
             },
         ])
@@ -45,11 +49,12 @@ export default function MyHeader() {
                 items={topNavItems}
                 mode="inline"
                 theme="light"
-                defaultSelectedKeys={['home']}
+                // defaultSelectedKeys={['home']}
+                selectedKeys={[location.pathname]}
             />
 
-            {isLogin && user && <Flex gap={'middle'} align={'center'} vertical>
-                <span style={{color: 'blue'}}>{JSON.parse(user).email}</span>
+            {isLogin && user && <Flex gap={'middle'} align={'center'}>
+                <span style={{color: 'white'}}>{JSON.parse(user).email}</span>
                 <Button
                     onClick={logoutHandler}
                     size={'small'}
